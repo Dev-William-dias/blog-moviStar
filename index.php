a97bfb7e990343c98829e9b48fae0d77d4ace4ff
Dev-William-dias <williampacifico41@gmail.com>
2025-12-20 18:35:20 -0300

    feat: create authentication page and add CSS

--- index.php
+++ index.php
@@ -1,67 +1,9 @@
 <?php
-    require_once("globals.php");
-    require_once("db.php");
+    require_once("templates/header.php");
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MovieStar</title>
-    <link rel="short icon" href="img/moviestar.icon">
-    <!--CSS-->
-    <link rel="stylesheet" href="css/style.css">
-    <!--bootstrap-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.8/css/bootstrap.css" integrity="sha512-zylce7fP6h4usg536JBTRj2rt7q22Z0qicHSlgSK53Irtfkz37ate3KCQ59du+aXZV6R3yyL2X1LyGKBEUMZaw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!--font awesome-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-<body>
-    <header>
-        <nav id="main-navbar" class="navbar navbar-expand-lg">
-            <a href="<?= $BASE_URL ?>" class="navbar-brand">
-                <img src="img/logo.svg" alt="MovieStar" id="logo">
-                <span id="moviestar-title">MovieStar</span>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation"><i class="fas fa-bars"></i></button>
-            <form action="" method="GET" id="search-form" class="d-flex align-items-center my-2 my-lg-0">   
-                <input type="search" name="q" id="search" class="form-control me-2" placeholder="Buscar Filmes" aria-label="Search">
-                <button class="btn btn-outline-secondary" type="submit">
-                    <i class="fas fa-search"></i>
-                </button>
-            </form>
-            <div class="collapse navbar-collapse" id="navbar">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
     <div id="main-container" class="container-fluid">
         <h1>Corpo do Site</h1>
     </div>
-    <footer id="footer">
-        <div id="social-container">
-            <ul>
-                <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
-                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-            </ul>
-        </div>
-        <div id="footer-links-container">
-            <ul>
-                <li><a href="#">Adicionar filme</a></li>
-                <li><a href="#">Adicionar critica</a></li>
-                <li><a href="#">Entrar / Registrar</a></li>
-            </ul>
-        </div>
-        <p>&copy; 2025 projeto curso</p>
-    </footer>
-    <!--bootstrap js-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.8/js/bootstrap.js" integrity="sha512-GKxQGmCBqm3YHCZTdqD3Bvsz+AHRObqztq6xDo+68527RxgVw7wF1TR5ZSBvtWiz5qzpOzUGGvxJbT/6UzzSOQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</body>
-</html>
+<?php
+    require_once("templates/footer.php");
+?>
